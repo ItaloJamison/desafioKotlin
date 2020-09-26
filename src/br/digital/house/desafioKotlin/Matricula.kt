9983c7f219a12669906563a0c9a8da9f87f8241a
Italo Jamison Félix Monteiro <42391885+ItaloJamison@users.noreply.github.com>
2020-09-26 18:21:40 -0300

    Criando Classe Matricula

---
+++ src/br/digital/house/desafioKotlin/Matricula.kt
@@ -0,0 +1,17 @@
+package br.digital.house.desafioKotlin
+
+import java.util.*
+
+// Data class contendo as informações:
+// aluno: Aluno
+// curso: Curso
+data class Matricula(
+        var aluno: Aluno,
+        var curso: Curso,
+){
+    // Atributo contendo a data atual
+    var data = Date()
+
+    // Reconstruição do ToString
+    override fun toString(): String = "${aluno} \n ${curso} \n Data: $data"
+}
